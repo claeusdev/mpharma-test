47281366c234d6d57e67342a2a5a4e0c397b83d4
Nana Adjei Manu <claeusdev@outlook.com>
2019-08-23 16:33:48 -0700

    define project structure

---
+++ src/components/Form.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Form() {
+  return <h1>Form Component</h1>;
+}
+
+
+export default Form;
---
+++ src/components/Product.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+function Product(){
+  return <h1>Product Component</h1>
+}
+
+export default Product;
