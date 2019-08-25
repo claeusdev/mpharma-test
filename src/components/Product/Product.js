de44c2af29febe3f429b9e29a2e00117f2e2b6b9
Nana Adjei Manu <claeusdev@outlook.com>
2019-08-24 18:18:09 -0700

    add function to calculate latest price

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,27 +1,36 @@
 import React, { useState } from "react";
 import Form from "../NewProductForm";
 import { ProductItem } from "../Styles";
+import moment from "moment";
 
 function Product({ product, deleteProduct }) {
   const [isEditing, setIsEditing] = useState(false);
   const toggleEditing = () => {
     setIsEditing(!isEditing);
   };
+
+  const calcLatestPrice = prices => {
+    let closeDate = prices.map(price => moment(price.date));
+    let latestPrice = prices.find(
+      price =>
+        price.date === moment.max(closeDate)._i);
+    return latestPrice.price;
+  };
   return (
     <>
       {isEditing ? (
         <Form product={product} />
       ) : (
-          <ProductItem key={product.id}>
-            <div className="details">
-              <h1 key={product.id}>
-            {product.name}</h1>
-            </div>
-            <div className="actions">
-              <span onClick={toggleEditing}>edit</span>
+        <ProductItem key={product.id}>
+          <div className="details">
+            <h1>{product.name}</h1>
+            <p>Latest Price: GHS {calcLatestPrice(product.prices)}</p>
+          </div>
+          <div className="actions">
+            <span onClick={toggleEditing}>edit</span>
             <span onClick={e => deleteProduct(e, product)}>delete</span>
-            </div>
-          </ProductItem>
+          </div>
+        </ProductItem>
       )}
     </>
   );
